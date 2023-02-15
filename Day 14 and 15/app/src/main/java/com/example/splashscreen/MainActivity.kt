aff1d134f2af2560f202dc5a3ea905c7f11060e0
Saumya Singh <saumya@Saumyas-MacBook-Pro.local>
2023-02-15 20:28:21 +0530

    Day 14 and 15 : Splash and Gradients

---
+++ Day 14 and 15/app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
---
+++ Day 14 and 15/app/src/main/java/com/example/splashscreen/SignUp.kt
@@ -0,0 +1,13 @@
+package com.example.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+    }
+}
