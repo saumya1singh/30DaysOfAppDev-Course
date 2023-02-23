74d93df8a4f2e42d6704ecf76b61e25aa5880c03
Saumya Singh <saumya@Saumyas-MacBook-Pro.local>
2023-02-23 13:04:53 +0530

    Lecture 18

---
+++ Day18ViewBinding/app/src/main/java/com/example/day18viewbinding/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.day18viewbinding
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.day18viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            if(binding.checkBox.isChecked){
+                // Open a New Screen
+            } else{
+                binding.checkBox.buttonTintList = ColorStateList.valueOf(Color.RED)
+                Toast.makeText(this, "Please accept the T&C", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
